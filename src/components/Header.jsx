45e5541a952376b3a650d08e5eac70bbcdda57b4
Haseeb Ashraf <haseeb.ashraf@psca.gop.pk>
2024-08-17 16:45:14 +0500

    main ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ export const Header = () => {
         <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
           <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Alchemative</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Alchemative main</span>
           </NavLink>
           <div className="flex md:order-2">
 
