907023efc6bd074e057478f850f83ad2e6e917f5
Haseeb Ashraf <haseeb.ashraf@psca.gop.pk>
2024-08-19 17:09:59 +0500

    update dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,24 @@ const App = () => {
 
       <Router>
         <ScrollToTop />
-        <Header />
 
-        <div className="dark:bg-slate-700">
-          <Routes>
-            <Route path="/" element={<MovieListPage apiPath="movie/now_playing" title="Home" />} />
-            <Route path="/popular" element={<MovieListPage apiPath="movie/popular" title="Popular / TMDB" />} />
-            <Route path="/toprated" element={<MovieListPage apiPath="movie/top_rated" title="Top Rated / TMDB" />} />
-            <Route path="/upcomming" element={<MovieListPage apiPath="movie/upcoming" title="Upcomming / TMDB" />} />
-            <Route path="/movie/:id" element={<MovieDetailPage />} />
-            <Route path="/search" element={<Search apiPath="search/movie" />} />
-            <Route path="*" element={<PageNotFound />} />
+        <div className="allignment flex flex-col min-h-screen justify-between">
+          <Header />
 
-          </Routes>
+          <div className="dark:bg-slate-700">
+            <Routes>
+              <Route path="/" element={<MovieListPage apiPath="movie/now_playing" title="Home" />} />
+              <Route path="/popular" element={<MovieListPage apiPath="movie/popular" title="Popular / TMDB" />} />
+              <Route path="/toprated" element={<MovieListPage apiPath="movie/top_rated" title="Top Rated / TMDB" />} />
+              <Route path="/upcomming" element={<MovieListPage apiPath="movie/upcoming" title="Upcomming / TMDB" />} />
+              <Route path="/movie/:id" element={<MovieDetailPage />} />
+              <Route path="/search" element={<Search apiPath="search/movie" />} />
+              <Route path="*" element={<PageNotFound />} />
+
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
       </Router>
 
 
